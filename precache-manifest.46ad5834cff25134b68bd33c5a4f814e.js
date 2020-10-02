bcad49bae515b2b78c517303af03ec2dd44dca67
carlos neme <carlosneme23@gmail.com>
2020-10-02 00:41:02 -0300

    Updates

---
+++ precache-manifest.46ad5834cff25134b68bd33c5a4f814e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a0727c6c2f152aa19e8d8654d81d206",
+    "url": "/PokemonApp/index.html"
+  },
+  {
+    "revision": "2ce4329696559908feac",
+    "url": "/PokemonApp/static/css/main.8c3b42f7.chunk.css"
+  },
+  {
+    "revision": "ef332a34697c7a741cd8",
+    "url": "/PokemonApp/static/js/2.ac3185df.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/PokemonApp/static/js/2.ac3185df.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2ce4329696559908feac",
+    "url": "/PokemonApp/static/js/main.061fd80e.chunk.js"
+  },
+  {
+    "revision": "116070310d1db235cc5e",
+    "url": "/PokemonApp/static/js/runtime-main.534d3c94.js"
+  }
+]);
